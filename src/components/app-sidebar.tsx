44668cf53bc3d256d31b64e5d77719336a83cbf1
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2025-03-31 17:31:16 -0400

    Initial Commit

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,94 @@
+'use client';
+
+import { BookOpen, Code, FileDown, GalleryVerticalEnd, HousePlug, Settings } from 'lucide-react';
+import * as React from 'react';
+
+import { NavMain } from '@/components/nav-main';
+import { Sidebar, SidebarContent, SidebarHeader, SidebarRail } from '@/components/ui/sidebar';
+import { TeamSwitcher } from './team-switcher';
+
+// This is sample data.
+const data = {
+  user: {
+    name: 'shadcn',
+    email: 'm@example.com',
+    avatar: '/avatars/shadcn.jpg',
+  },
+  teams: [
+    {
+      name: 'Content Export',
+      logo: GalleryVerticalEnd,
+    },
+  ],
+  navMain: [
+    {
+      title: 'Dashboard',
+      url: '/',
+      icon: HousePlug,
+    },
+    {
+      title: 'Content',
+      url: '#',
+      icon: FileDown,
+      isActive: true,
+      items: [
+        {
+          title: 'Export Tool',
+          url: '/content/export',
+        },
+        {
+          title: 'Import Tool',
+          url: '/content/import',
+        },
+        {
+          title: 'Copilot',
+          url: '/content/copilot',
+        },
+      ],
+    },
+
+    {
+      title: 'Configuration',
+      url: '#',
+      icon: Settings,
+      isActive: true,
+      items: [
+        {
+          title: 'Sitecore Instances',
+          url: '/settings/instance',
+        },
+        {
+          title: 'API Tokens',
+          url: '/settings/config',
+        },
+      ],
+    },
+    {
+      title: 'Documentation',
+      url: 'https://github.com/estockwell-alpert/Content-Export-Tool-for-XM-Cloud?tab=readme-ov-file#using-the-application',
+      icon: BookOpen,
+    },
+    {
+      title: 'Source Code',
+      url: 'https://github.com/estockwell-alpert/Content-Export-Tool-for-XM-Cloud',
+      icon: Code,
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      {/* <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter> */}
+      <SidebarRail />
+    </Sidebar>
+  );
+}
